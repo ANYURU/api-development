acd872eaa676229454f4dd2c6cb4948c6d534263
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2022-12-01 15:06:45 +0300

    components

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -24,9 +24,6 @@ def loginPage(request):
     if request.method == "POST":
         username = request.POST.get('username').lower()
         password = request.POST.get('password')
-
-        print('Username:', username, sep=" ")
-        print('Password:', password, sep=" ")
 
         try: 
             user = User.objects.get(username=username)
@@ -77,12 +74,28 @@ def home(request):
 
     room_count = rooms.count()
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics':topics, 'room_count': room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {'rooms': rooms, 'topics':topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = { 'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        print(request.POST.dict() )
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+
+        room.participants.add(request.user)  
+        return redirect('room', pk=room.id)
+        
+    context = { 'room': room, 'room_messages':room_messages,'participants': participants}
     return render(request, 'base/room.html', context)
 
 # @login_required(login_required='/login')
@@ -123,9 +136,21 @@ def updateRoom(request, pk):
 def deleteRooom(request, pk):
     room = Room.objects.get(id=pk)
     if request.user != room.host:
-        return HttpResponse("You are not allowed here!!")
+        return HttpResponse("Not Allowed")
 
     if request.method == 'POST':
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+@login_required(login_url='delete-message')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse("Not allowed")
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
+
