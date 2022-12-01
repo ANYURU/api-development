995506de8ba6dd364de04b5e7d74f61a4de686d0
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2022-12-01 16:00:33 +0300

    User profile

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>', views.userProfile, name="user-profile"),
     path('create-room/', views.create_room, name="create-room"), 
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>/', views.deleteRooom, name="delete-room"),
